7c84bb42b24618b63616986744c64135974de92d
George <7GBars@gmail.com>
2024-03-14 12:41:59 +0300

    -добавить блок banner, -установить библиотеку animate.css

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import {NavBar} from "./components/navBar/NavBar";
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Banner} from "./components/banner/Banner";
 function App() {
   return (
     <div className="App">
       <NavBar/>
+      <Banner/>
     </div>
   );
 }
